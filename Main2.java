20414ccfce8d9a154a166a41d4f19ee8a33d9148
Tungalead <ssanogom@gmail.com>
2019-03-04 20:19:56 +0000

    les deux modifier ensemble last

--- Main2.java
+++ Main2.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World. I am your King and Good Luck To Everyone");
-	    System.out.println("J'ai modifier");
     }
 }
