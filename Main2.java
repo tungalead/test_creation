2e9d9bb1fdcb32005ce8144f384f44d411a789e8
Tungalead <ssanogom@gmail.com>
2019-03-04 20:10:42 +0000

    mise a jour complete

--- Main2.java
+++ Main2.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World. I am your King and Good Luck To Everyone");
-        System.out.println("Seconde Fois d'Afficher du Texte");
-	//System.out.println("Troisieme Fois d'Afficher du le meme message ");
 	    System.out.println("J'ai modifier");
     }
 }
