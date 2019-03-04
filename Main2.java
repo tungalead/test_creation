d8d50d9d57fa4240a2e5421597e7ad26baefb27a
Tungalead <ssanogom@gmail.com>
2019-03-04 20:06:08 +0000

    supp ligne et ajout ligne

--- Main2.java
+++ Main2.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello World. I am your King and Good Luck To Everyone");
         System.out.println("Seconde Fois d'Afficher du Texte");
-		System.out.println("Troisieme Fois d'Afficher du le meme message ");
+	//System.out.println("Troisieme Fois d'Afficher du le meme message ");
+	    System.out.println("J'ai modifier");
     }
 }
