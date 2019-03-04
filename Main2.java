f53d246d7b156ac9f15e106110ba11d085aee087
Tungalead <ssanogom@gmail.com>
2019-03-04 19:53:15 +0000

    ajout de Main2.java a la source

---
+++ Main2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World. I am your King and Good Luck To Everyone");
+        System.out.println("Seconde Fois d'Afficher du Texte");
+		System.out.println("Troisieme Fois d'Afficher du le meme message ");
+    }
+}
